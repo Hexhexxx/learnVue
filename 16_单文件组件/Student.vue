8f14619c34c07928d9013062f8f03831b6288315
Hex <hexhexxx@163.com>
2023-01-08 19:57:53 +0800

    2023-1-8

---
+++ 16_单文件组件/Student.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="demo">
+    <!-- 组件的结构 -->
+    <h2>学生名称:{{name}}</h2>
+    <h2>学生年龄:{{age}}</h2>
+    <button @click="showName">显示信息</button>
+  </div>
+</template>
+
+<script>
+//组件交互相关代码
+  export default{
+    name:'School',
+    data () {
+      return {
+        name: '张三',
+        age: 18
+      }
+    },
+    methods: {
+      showName(){
+        alert(this.name)
+      }
+    },
+  }
+    
+</script>
+
+<style>
+.demo{
+  background-color: skyblue;
+}
+</style>
---
+++ 17_脚手架/test01/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+</script>
+
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<style>
+</style>
